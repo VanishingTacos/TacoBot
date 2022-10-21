b3e1e0a40b4c62b2d7f77674ef4ecef24bd6df26
VanishingTacos <devonstuper@gmail.com>
2022-10-21 13:07:28 -0400

    twitch notifs

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -1,5 +1,6 @@
 import json
 from tabnanny import check
+from turtle import width
 import discord
 from discord.ext import commands, tasks
 from matplotlib.image import thumbnail
@@ -32,6 +33,15 @@ def save_stream_state(stream_state):
     with open('./JSON/stream_state.json', 'w') as f:
         json.dump(stream_state, f)
 
+def create_embed(title, userid, viewer_count, get_user_profile_pic):
+    embed = discord.Embed(title = title, url = f"https://twitch.tv/{userid}")
+    embed.add_field(name = "Viewers", value = viewer_count)
+    embed.set_author(name = userid, icon_url = get_user_profile_pic)
+    embed.set_image(url = f"https://static-cdn.jtvnw.net/previews-ttv/live_user_{userid}-320x180.jpg")
+    embed.set_thumbnail(url = get_user_profile_pic)
+    return embed
+
+
 # Authentication with Twitch API.
 client_id =  os.environ.get("TWITCH_CLIENT_ID")
 client_secret = os.environ.get("TWITCH_CLIENT_SECRET")
@@ -58,65 +68,55 @@ API_HEADERS = {
 
 # Returns true if online, false if not.
 def checkuser(userid):
-    try:
-        # Get Twitch user infomation
-        get_user_url = TWITCH_USER_ENDPOINT + userid
-        get_user_req = requests.Session().get(get_user_url, headers=API_HEADERS)
-        get_user_json = get_user_req.json()
-        get_user_profile_pic = get_user_json['data'][0]['profile_image_url']
-        # Get Twitch stream data
-        get_stream_url = TWITCH_STREAM_ENDPOINT + userid
-        get_stream_req = requests.Session().get(get_stream_url, headers=API_HEADERS)
-        get_stream_json = get_stream_req.json()
-        print(get_stream_json)
-        if get_stream_json['data']:
-            title = get_stream_json['data'][0]['title']
-            game_name = get_stream_json['data'][0]['game_name']
-            viwer_count = get_stream_json['data'][0]['viewer_count']
-            if userid not in load_stream_state():
-                stream_state[userid] = []
-                stream_state[userid].append({
-                    'is_live': True,
-                    'title': title,
-                    'game_name': game_name
-
-                    })
-                save_stream_state(stream_state)
-                embed = discord.Embed(title=title, description=viwer_count)
-                embed.set_thumbnail(url=get_user_profile_pic)
-                return embed
-                # return [True, title, game_name]
-            elif userid in load_stream_state():
-                if stream_state[userid][0]['is_live'] == True:
-                    return True
-                elif stream_state[userid][0]['is_live'] == False:
-                    stream_state[userid][0]['is_live'] = True
-                    stream_state[userid][0]['title'] = title
-                    stream_state[userid][0]['game_name'] = game_name
-                    save_stream_state(stream_state)
-                    return [True, title, game_name]
-        else:
-            if userid not in load_stream_state():
-                stream_state[userid] = []
-                stream_state[userid].append({
-                    'is_live': False,
-                    'title': None,
-                    'game_name': None
+    # Get Twitch user infomation
+    get_user_url = TWITCH_USER_ENDPOINT + userid
+    get_user_req = requests.Session().get(get_user_url, headers=API_HEADERS)
+    get_user_json = get_user_req.json()
+    get_user_profile_pic = get_user_json['data'][0]['profile_image_url']
+    # Get Twitch stream data
+    get_stream_url = TWITCH_STREAM_ENDPOINT + userid
+    get_stream_req = requests.Session().get(get_stream_url, headers=API_HEADERS)
+    get_stream_json = get_stream_req.json()
+    if get_stream_json['data']:
+        title = get_stream_json['data'][0]['title']
+        game_name = get_stream_json['data'][0]['game_name']
+        viewer_count = get_stream_json['data'][0]['viewer_count']
+        if userid not in load_stream_state():
+            stream_state[userid] = []
+            stream_state[userid].append({
+                'is_live': True,
+                'title': title,
+                'game_name': game_name
 
                 })
+            save_stream_state(stream_state)
+            return create_embed(title, userid, viewer_count, get_user_profile_pic)
+        elif userid in load_stream_state():
+            if stream_state[userid][0]['is_live'] == True:
+                return True
+            elif stream_state[userid][0]['is_live'] == False:
+                stream_state[userid][0]['is_live'] = True
+                title = get_stream_json['data'][0]['title']
+                stream_state[userid][0]['title'] = title
+                stream_state[userid][0]['game_name'] = game_name
+                viewer_count = get_stream_json['data'][0]['viewer_count']
                 save_stream_state(stream_state)
-            elif userid in load_stream_state():
-                if stream_state[userid][0]['is_live'] == True:
-                    stream_state[userid][0]['is_live'] = False
-                    save_stream_state(stream_state)
-                return False
+                return create_embed(title, userid, viewer_count, get_user_profile_pic)
+    else:
+        if userid not in load_stream_state():
+            stream_state[userid] = []
+            stream_state[userid].append({
+                'is_live': False,
+                'title': None,
+                'game_name': None
 
-    except Exception as e:
-        print("Error checking user: ", e)
-        print(traceback.format_exc())
-        return False
-    except IndexError:
-        return False
+            })
+            save_stream_state(stream_state)
+        elif userid in load_stream_state():
+            if stream_state[userid][0]['is_live'] == True:
+                stream_state[userid][0]['is_live'] = False
+                save_stream_state(stream_state)
+            return False
 
 
 
@@ -127,8 +127,16 @@ class twitch(commands.Cog):
 
     @tasks.loop(seconds=5)
     async def live_notifs_loop(self):
-        get_embed = checkuser('nandre')
-        await self.bot.get_channel(911372583235092485).send(embed = get_embed)
+        get_embed = checkuser('vanishingtacos')
+        print(get_embed)
+
+        if not get_embed:
+            pass
+        else:
+            try:
+                await self.bot.get_channel(911372583235092485).send(embed = get_embed)
+            except:
+                pass
     
     @commands.Cog.listener()
     async def on_ready(self):
